2b4e46809d0023085d05c17420e2df1c2b359d7d
Vinicius Sousa <viniciusxsousa@outlook.com>
2022-11-24 23:39:00 -0300

    adicionando principal.js

---
+++ moveis/principal.js
@@ -0,0 +1,9 @@
+var banners = ['Os melhores do Brasil!', 'Qualidade e preço baixo!'];
+var bannerAtual = 0;
+
+function trocarBanner() {
+    bannerAtual = (bannerAtual + 1) % 2;
+    document.querySelector('#mensagem').textContent = banners[bannerAtual];
+}
+
+setInterval(trocarBanner, 2000);
